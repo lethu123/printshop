e5c758b0da9916facfd307fc15df29d95a42271b
lethithu <lethu210398@gmail.com>
2024-02-06 17:48:00 +0700

    update description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.scss';
 
 export const metadata: Metadata = {
   title: "Nhà in Dương Phong",
-  description: "make by me",
+  description: "Công ty TNHH Quảng cáo & Thương mại Dương Phong – DPA – Sự lớn mạnh của bạn là thành công của chúng tôi",
   keywords: ['nhà in', 'Dương phong', 'xưởng in', 'xưởng in Dương Phong'],
   openGraph: {
     images: 'http://dpa.vn/wp-content/uploads/2016/07/dpa-logo.png' ,
