b0b37bdb8aeeb9e7204e28cbab969674671bcbeb
lethithu <lethu210398@gmail.com>
2024-02-06 15:00:28 +0700

    autoplay list  product

--- src/components/home/ProductComponent.tsx
+++ src/components/home/ProductComponent.tsx
@@ -52,11 +52,12 @@ const ProductComponent = () => {
       </div>
       <Slider
         className='center'
-        infinite={true}
-        centerPadding='60px'
+        infinite={true} 
         slidesToShow={3}
         slidesToScroll={3}
-        speed={1000}
+        autoplay={true}
+        autoplaySpeed={3000}
+        speed={1500}
         nextArrow={<SampleNextArrow />}
         prevArrow={<SamplePrevArrow />}
         responsive={[
